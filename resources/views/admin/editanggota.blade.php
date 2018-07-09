597b1ebbb801c03913c064df7ab8e2243af56f9d
DESKTOP-SQS1BD1\Rezky <rezkyputri@mhs.unesa.ac.id>
2018-07-09 15:51:22 +0700

    menambahkan form edit anggota

---
+++ resources/views/admin/editanggota.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="row">
+                                <h4 class="col-md-8 card-title">Edit Data Anggota</h4>
+                                <div class="col-md-4 pl-1 ">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control pull-right" disabled="" placeholder="ID Anggota disabled">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <form>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Nama Lengkap</label>
+                                            <input type="text" class="form-control" placeholder="Nama">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label >Alamat</label>
+                                            <input type="text" class="form-control" placeholder="Alamat">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>No. Identitas (KTP/SIM/KTM)</label>
+                                            <input type="text" class="form-control" placeholder="No. Identitas">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>No. Telp/HP</label>
+                                            <input type="text" class="form-control" placeholder="No. Telp/HP">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Jenis Kelamin</label>
+                                            <select class="form-control" id="pekerjaan">
+                                                <option>--Pilih Jenis Kelamin--</option>
+                                                <option>Laki-laki</option>
+                                                <option>Perempuan</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Pekerjaan</label>
+                                            <select class="form-control" id="pekerjaan">
+                                                <option>--Pilih Pekerjaan--</option>
+                                                <option>PNS</option>
+                                                <option>Pelajar/Mahasiswa</option>
+                                                <option>Karyawan Swasta</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="{{route('admin/anggota')}}" type="submit" class="btn btn-info btn-fill pull-right">Update Anggota</a>
+                                <div class="clearfix"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
