dd1b03212dd9975b46ce4ce6e913d2a371fd9d42
DESKTOP-SUKEJGH\rezky <rezkyputri@mhs.unesa.ac.id>
2018-07-11 16:01:03 +0700

    edit user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('alamat');
+            $table->integer('hak_akses');
             $table->rememberToken();
             $table->timestamps();
         });
